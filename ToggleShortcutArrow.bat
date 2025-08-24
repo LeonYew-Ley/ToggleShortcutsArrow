4df188481a34e8c77d548a19e3aa44b4e4d576cd
LeonYew <learngenius@outlook.com>
2025-08-24 18:13:01 +0800

    更新README

--- ToggleShortcutArrow.bat
+++ ToggleShortcutArrow.bat
@@ -1,3 +1,37 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWH3eyGE/LBJaSzSQM2G/BaEP1Mr6++mPnnkSU+UzfbzT1aaaI/UH+WT2Z5k66m5ImcUfHBpKexy/IAY3pg4=
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDdRTRaMAE+1EbsQ5+n//NaTp14JaNYwf4jX34eGL/IH6VDwZtgozn86
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 
 @echo off
 :: 检查是否以管理员身份运行
